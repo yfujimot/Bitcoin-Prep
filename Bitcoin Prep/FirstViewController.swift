2f40c2c7cd9e33916496d259c07ff85c8b35fc52
Yoshio Fujimoto <Yoshi@Yoshios-MacBook-Pro.local>
2014-08-04 11:05:21 -0400

    Initial Commit

---
+++ Bitcoin Prep/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Bitcoin Prep
+//
+//  Created by Yoshio Fujimoto on 8/4/14.
+//  Copyright (c) 2014 Yoshio Fujimoto. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
